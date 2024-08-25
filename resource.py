9afa00f1fd490fd2d5e76b5cd1831f464dfa64d1
Lucy77m <lii893304.gmail.com>
2024-08-25 18:13:11 +0900

    GoGOGO

---
+++ resource.py
@@ -0,0 +1,15 @@
+
+piece_images = {
+    "r": "img/black/rook.webp",
+    "n": "img/black/knight.webp",
+    "b": "img/black/bishop.webp",
+    "q": "img/black/queen.webp",
+    "k": "img/black/king.webp",
+    "p": "img/black/pawn.webp",
+    "R": "img/white/rook.webp",
+    "N": "img/white/knight.webp",
+    "B": "img/white/bishop.webp",
+    "Q": "img/white/queen.webp",
+    "K": "img/white/king.webp",
+    "P": "img/white/pawn.webp"
+}
